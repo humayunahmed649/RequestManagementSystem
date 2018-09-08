06442fa85b6326a0a29beb9ce60c4c73dd7e309f
chittoranjan <chittoranjan.sarker@gmail.com>
2018-09-09 04:02:44 +0600

    Send requisition datatable addes

--- RMS.Models/EntityModels/Feedback.cs
+++ RMS.Models/EntityModels/Feedback.cs
@@ -11,11 +11,14 @@ namespace RMS.Models.EntityModels
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         [StringLength(250)]
         public string CommentText { get; set; }
+
         public int RequisitionId { get; set; }
         public Requisition Requisition { get; set; }
+
         public int? FeedBackId { get; set; }
 
     }
