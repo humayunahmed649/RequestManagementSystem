d3cb8b48466aaf9892ba62bc6552343867ba68f8
parvezahmed <humayunahmed649@gmail.com>
2018-10-01 06:28:23 -0500

    Assign Requisition Calendar

---
+++ RMS.App/Controllers/ScheduleController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RMS.BLL.Contracts;
+
+namespace RMS.App.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private IAssignRequisitionManager _assignRequisitionManager;
+
+        public ScheduleController(IAssignRequisitionManager assignRequisitionManager)
+        {
+            this._assignRequisitionManager = assignRequisitionManager;
+        }
+
+        // GET: Schedule
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetAssignRequisition()
+        {
+            var requisitions = _assignRequisitionManager.GetAll().ToList();
+            return new JsonResult() {Data = requisitions,JsonRequestBehavior = JsonRequestBehavior.AllowGet};
+        }
+    }
+}
